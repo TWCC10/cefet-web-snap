ee60c989e9496af0585fae9e8ee47b1005adcec2
Thiago <thiagowolff10@gmail.com>
2022-07-28 23:35:20 -0300

    Término de parte 1 e começo do exercício 2

---
+++ script/balaozinho.js
@@ -0,0 +1,11 @@
+let balaozinhos = document.querySelectorAll('.marcacao');
+
+for(let eLbalao of balaozinhos)
+{
+    eLbalao.addEventListener('mouseover',mouseHover);
+
+}
+
+function mouseHover(e){
+
+}
